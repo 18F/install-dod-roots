a421ebb3451c0bf7d016f1c2f1f91a5270ea8515
Greg Walker <michael.walker@gsa.gov>
2017-04-06 11:17:24 -0500

    Initial

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+curl -s http://iasecontent.disa.mil/pki-pke/Certificates_PKCS7_v5.0u1_DoD.zip > dod_root_certs.zip
+unzip -o -qq dod_root_certs.zip
+cd Certificates_PKCS7_v5.0u1_DoD
+
+sudo -v -p "Please enter your computer password to install the DoD Root Certificates into your keychain: "
+
+sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login.keychain DoD_Root_CA_2__0x05__DoD_Root_CA_2.cer
+sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login.keychain DoD_Root_CA_3__0x01__DoD_Root_CA_3.cer
+sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login.keychain DoD_Root_CA_4__0x01__DoD_Root_CA_4.cer
+
+sudo -k
+
+cd ..
+rm -rf Certificates_PKCS7_v5.0u1_DoD
+rm dod_root_certs.zip
